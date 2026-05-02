1ceac61f5981488494239ea713b2eae76d6b67a7
Ayushisinghcs28 <ayushisingh20132023@gmail.com>
2026-05-02 13:49:34 +0530

    WeatherApp  with hidden API key

---
+++ script.js
@@ -0,0 +1,33 @@
+async function getWeather() {
+    let city = document.getElementById("cityInput").value;
+
+    if (city === "") {
+        alert("Enter city name!");
+        return;
+    }
+
+   let apiKey = API_KEY;
+
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+
+        if (data.cod === "404") {
+            document.getElementById("weatherResult").innerHTML = "City not found!";
+            return;
+        }
+
+        let result = `
+            <h3>${data.name}</h3>
+            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Weather: ${data.weather[0].main}</p>
+        `;
+
+        document.getElementById("weatherResult").innerHTML = result;
+
+    } catch (error) {
+        document.getElementById("weatherResult").innerHTML = "Error fetching data!";
+    }
+}
